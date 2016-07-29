afe26709e27f13b2d940c20fb6813d14ac977b7d
Adam Li <adam2392@gmail.com>
2016-07-29 11:14:45 -0400

    initial commit.

---
+++ public/js/drawmap.js
@@ -0,0 +1,171 @@
+/*
+    GLOBAL VARS
+*/
+var width = 700;
+var mapRatio = 0.75;
+var height = width * mapRatio;
+var embiggen = 1.00;
+var mapVOffset = 0.05
+var mapHeight = height - mapVOffset
+
+var svg = d3.select("#map").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// var region = document.getElementById("regions")
+var region = document.getElementById("regions").value
+var census_file = "../data/" + "delmarva" + "_census_data.csv";
+var shape_file = "../data/" + "balt_city" + "_geo_data.json";
+
+var cs = ['rgb(255,255,217)','rgb(237,248,177)','rgb(199,233,180)','rgb(127,205,187)','rgb(65,182,196)','rgb(29,145,192)','rgb(34,94,168)','rgb(37,52,148)','rgb(8,29,88)']
+var q = d3.scaleQuantile()
+    .range(cs)
+
+var loLim = 4;
+var hiLim = 15;
+var loIdx = 2;
+var hiIdx = 10;
+
+var dataMap = d3.map(); // Create an array, see https://github.com/mbostock/d3/wiki/Arrays
+
+var data = [];
+var counts = [];
+var dataKeys = [];
+
+/* 
+    AREA FOR FUNCTIONS TO CREATE MAP
+*/
+function makeMap() {
+    // debug statements
+    console.log("Creating the map... makeMap()");
+	svg.remove();
+
+    // create the svg inside "map" div
+	svg = d3.select("#map").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    // get the corresponding region
+    var region = document.getElementById("regions").value
+
+    // initialize file locations for corresponding region
+    // var census_file = "../data/" + region + "_census_data.csv";
+    // var shape_file = "../data/" + region + "_geo_data.json";
+    var census_file = "../data/" + "delmarva" + "_census_data.csv";
+    var shape_file = "../data/" + "balt_city" + "_geo_data.json";
+
+    // wait for json/csv of shape_files/census_files to be loaded
+    // -> call main function
+    queue()
+        .defer(d3.json, shape_file)
+        .defer(d3.csv, census_file)
+        .await(ready);
+}
+queue()
+    .defer(d3.json, shape_file)
+    .defer(d3.csv, census_file)
+    .await(ready);
+
+function ready(error, shapes, csvData) {
+    if(error) throw error;
+    console.log("Running ready()...");
+    loadCSVData(csvData);
+// Create a unit projection.
+    var projection = d3.geoMercator()
+        .scale(1)
+        .translate([0, 0]);
+
+    // Create a path generator.
+    var path = d3.geoPath()
+        .projection(projection);
+
+    // Compute the bounds of a feature of interest, then derive scale & translate.
+    var b = path.bounds(shapes),
+    s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / mapHeight),
+    t = [(width - s * (b[1][0] + b[0][0])) / 2, mapVOffset*mapHeight + (mapHeight - s * (b[1][1] + b[0][1])) / 2];
+
+    // Update the projection to use computed scale & translate.
+    projection
+        .scale(s)
+        .translate(t);
+
+    svg.selectAll("path")
+        .data(shapes.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class","cc")
+        .attr("fill", function(d) {
+            if (dataMap.get(d.id) == 0 || isNaN(dataMap.get(d.id))) {
+                return "#eee";
+            }          
+            return q(dataMap.get(d.id));
+        })
+        .on("mouseover", function(d) {
+            var blockGroup = d3.select(this);
+            blockGroup.classed('active', true);
+            this.parentNode.appendChild(this);
+        })
+        .on("mouseout", function(d) {
+            var blockGroup = d3.select(this);
+            blockGroup.classed('active', false);
+        })
+        .attr("vector-effect", "non-scaling-stroke");
+    
+// Start Legend -----------------------
+      // svg.append("text")
+      //   .attr("id", "leftlabel")
+      //   .style({"font-size":"18px","font-weight":"bold", "fill":"#333", "text-anchor": "end"})
+      //   .attr("x", 40)
+      //   .attr("y", 20)
+      //   .text("0%"); 
+
+      // svg.append("text")
+      //   .attr("id", "rightlabel")
+      //   .style({"font-size":"18px","font-weight":"bold", "fill":"#333"})
+      //   .attr("x", 510)
+      //   .attr("y", 20)
+      //   .text("100%");
+
+      svg.selectAll("rect")
+        .data(cs)
+        .enter()
+        .append("rect")
+        .attr("x", function(d,i){
+          return 50 + (i * 50);
+        })
+        .attr("y", 10)
+        .attr("width", 50)
+        .attr("height", 10)
+        .attr("fill", function(d) {return d})
+// End legend -----------------------
+}
+
+function loadCSVData(csvData) {
+    data = csvData;
+    counts = [];
+    var dataKeys = Object.keys(data[0]); // get the first column, which are the keys
+    dataKeys.shift();                   // shift out the objectID = fips key
+
+    console.log(dataKeys);
+    csvData.map(function(d){
+        var count = 0;
+        var total = 0;
+        for (var i=loIdx; i < hiIdx + 1; i++) {
+            count += +d[dataKeys[i]];
+        };
+        for(var i = 0; i < dataKeys.length; i++){
+            total += +d[dataKeys[i]];
+        }
+
+        var pctCount = 1.0 * count / total;
+        counts.push(pctCount);
+        dataMap.set(d.fips, pctCount);
+    })
+    var filtered = counts.filter(isBigEnough); // Remove 0 elements
+    q.domain([0, 1]); // adjust domain of quantiles
+}
+
+function isBigEnough(value) {
+  return value > 0;
+}
